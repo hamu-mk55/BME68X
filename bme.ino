986268413ae52858a4afcf3066779340b57aa024
hamu-mk55 <153355786+hamu-mk55@users.noreply.github.com>
2024-08-13 16:47:30 +0900

    Add files via upload

--- bme.ino
+++ bme.ino
@@ -10,6 +10,7 @@
 
 #define NEW_GAS_MEAS (BME68X_GASM_VALID_MSK | BME68X_HEAT_STAB_MSK | BME68X_NEW_DATA_MSK)
 #define MEAS_DUR 140
+#define MEAS_NUM 9
 
 // CORE2
 #define SDA_PIN 32
@@ -23,6 +24,14 @@
 
 #include <math.h>
 Bme68x bme;
+
+typedef struct _sensor_data{
+  float temperature;
+  float humid;
+  float pressure;
+  float sensor[MEAS_NUM];
+};
+_sensor_data sd;
 
 // initialize
 void setup(void)
@@ -39,7 +48,7 @@ void setup(void)
 
   while (!Serial)
   {
-    delay(100);
+    delay(140);
   }
 
   /* initializes the sensor based on SPI library */
@@ -96,12 +105,10 @@ void loop(void)
       nFieldsLeft = bme.getData(data);
       if (data.status == NEW_GAS_MEAS)
       {
-
         // for display
         if(data.gas_index==0){
           M5.Lcd.clearDisplay();
           M5.Lcd.setCursor(0,0);
-          M5.Lcd.println(String(data.gas_index));
           M5.Lcd.println("tempe: " + String(data.temperature));
           M5.Lcd.println("humid: " + String(data.humidity));
           M5.Lcd.println("press: " + String(data.pressure));
@@ -109,21 +116,42 @@ void loop(void)
         }
         
         // for serial-output
-        Serial.print(String(data.gas_index)+",");
-        Serial.print(String(data.temperature) + ",");
-        Serial.print(String(data.humidity) + ",");
-        Serial.print(String(data.pressure) + ",");
+        if(data.gas_index==0){
+          // Serial.print("data,");
+          // Serial.print(String(data.temperature) + ",");
+          // Serial.print(String(data.humidity) + ",");
+          // Serial.print(String(data.pressure/100,2) + ",");
 
+          sd.temperature = data.temperature;
+          sd.pressure = data.pressure;
+          sd.humid = data.humidity;
+
+          for (int i=0; i < MEAS_NUM; i++){
+            sd.sensor[i] = 0;
+          }  
+        }
+        
         float current = log(data.gas_resistance);
-        Serial.print(String(current,3)+",");
-        Serial.println();
+        sd.sensor[data.gas_index] = current;
 
+        if(data.gas_index==MEAS_NUM -1 ){
+          Serial.print("data");Serial.print(",");
+          Serial.print(sd.temperature);Serial.print(",");
+          Serial.print(sd.humid);Serial.print(",");
+          Serial.print(sd.pressure);Serial.print(",");
+
+          for (int i=0; i < MEAS_NUM; i++){
+            Serial.print(String(sd.sensor[i],3));Serial.print(",");
+          }          
+          Serial.println();
+        }
+          
       }
     } while (nFieldsLeft);
   } else {
-    M5.Lcd.clearDisplay();
-    M5.Lcd.setCursor(0,0);
-    M5.Lcd.println("....No DATA");
-    delay(30);
+    //M5.Lcd.clearDisplay();
+    //M5.Lcd.setCursor(0,0);
+    //M5.Lcd.println("....No DATA");
+    //delay(30);
   }
 }
